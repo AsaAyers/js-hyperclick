f9d80dcd0153616de495491ba5a13433c58b1386
Asa Ayers <Asa@AsaAyers.com>
2015-10-24 10:08:58 -0400

    Gather requires

--- spec/make-cache-spec.js
+++ spec/make-cache-spec.js
@@ -19,9 +19,29 @@ describe("makeCache.parseCode", function() {
                 // 4
             }
         }())
-        `)
+        `).scopes
 
-        expect(actual.scopes.length).toBe(4)
+        expect(actual.length).toBe(4)
+    })
+
+    it("Gathers requires", function() {
+        const actual = parseCode(`
+        const foo = require('./foo')
+        const badRequire = require(foo.name)
+        const { bar } = require('./bar')
+
+        import someDefault, { named } from './other'
+
+        `).externalModules
+
+        expect(actual[0].name).toBe('foo')
+        expect(actual[0].module).toBe('./foo')
+        expect(actual[1].name).toBe('bar')
+        expect(actual[1].module).toBe('./bar')
+        // expect(actual[2].name).toBe('someDefault')
+        // expect(actual[2].module).toBe('./other')
+        // expect(actual[3].name).toBe('named')
+        // expect(actual[3].module).toBe('./other')
     })
 
 
