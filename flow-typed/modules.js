4334dc6e9618631ed7de1212f150088bf68ae7fe
Asa Ayers <Asa@AsaAyers.com>
2017-04-26 13:19:04 -0700

    Fix my Range vs Atom's Range in lib/js-hyperclick.js

--- flow-typed/modules.js
+++ flow-typed/modules.js
@@ -3,17 +3,45 @@
 
 declare var atom: Object;
 
-declare class Subscription {
+declare class Disposable {
   dispose(): void;
 }
 
 declare module 'atom' {
-    declare var Range: any;
+    declare type Point = {
+        toArray: () => [number, number]
+    }
+    declare class Range {
+        start: Point,
+        end: Point,
+    }
+    declare type Buffer = {
+        characterIndexForPosition: (position: Point) => number,
+        positionForCharacterIndex: (index: number) => any,
+        getTextInRange: (range: Range) => string,
+    }
     declare class CompositeDisposable{
-      add(observable: Subscription): void;
-      dispose(): void;
+        add(...observable: Array<Disposable>): void;
+        dispose(): void;
+    }
+    declare type TextEditor = {
+        getPath: () => string,
+        getGrammar: () => {
+            scopeName: string
+        },
+        getBuffer: () => Buffer,
+        setCursorBufferPosition: (point: Point) => void,
+        scrollToCursorPosition: () => void,
+        getText: () => string,
+        onDidStopChanging: (cb: () => void) => any
     }
 }
+
+
+
+
+
+
 
 declare module 'shell' {
     declare var openExternal: (string) => void
